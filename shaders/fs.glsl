08a41e73a573fd02565eedf898da26e5d98089ca
Matteo <amerenamatteo@gmail.com>
2022-01-03 20:54:09 +0100

    uv rotation (to_fix)

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -6,15 +6,24 @@ in vec2 fsUV;
 in vec3 fs_pos; 
 
 uniform sampler2D in_texture;
+uniform sampler2D AO_texture;
+uniform mat4 tMatrix;
+
+
 uniform bool isStand;
 
 out vec4 outColor;
 
 void main() {
+    vec2 uv = (tMatrix * vec4(fsUV.x, fsUV.y, 0.0, 1.0)).xy;
+
     if(isStand){
-        outColor = vec4(0.0, 0.0, 0.0, 1.0);
+        outColor = vec4(1.0, 0.3, 0.2, 1.0);
     }
-    else{
-        outColor = texture(in_texture, fsUV);
-    }
+    else
+    {
+   vec4 color0 = texture(in_texture, uv);
+   vec4 color1 = texture(AO_texture, uv);
+   outColor = color0 * color1;    
+   }
 }
