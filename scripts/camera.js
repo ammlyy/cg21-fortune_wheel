db331018e2f9d82067cc69923200079a39abd517
Gabriele Stucchi <gabriele.stucchi@mail.polimi.it>
2022-01-06 16:39:22 +0100

    Camera adjusted

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,9 +1,10 @@
 const CAMERA_TOP_OMEGA = 105;
 const CAMERA_BOTTOM_OMEGA = 10;
 const CAMERA_SPHERE_RADIUS = 3.5;
-const MAX_DEPTH = 3;
-const MIN_DEPTH = 0.4;
-const Y_DEFAULT = 5;
+const MAX_DEPTH = 2.5;
+const MIN_DEPTH = 0.5;
+const Y_WHEEL = 5.0;
+const Z_WHEEL = 1.0;
 const DELTA = 2
 
 // camera diff vector: updated at event keydown/up and read at each frame
@@ -11,9 +12,9 @@ var camera_diff = { x: 0, y: 0};
 // camera angles
 var camera_angles = { phi: 90.0, omega: 90 };
 var camera_depth = 1;
-var camera_pos = [0.0, Y_DEFAULT, CAMERA_SPHERE_RADIUS];
+var camera_pos = [0.0, Y_WHEEL, Z_WHEEL + CAMERA_SPHERE_RADIUS];
 
-var camera_target = [0.0, Y_DEFAULT, 0.0];
+var camera_target = [0.0, Y_WHEEL, Z_WHEEL];
 var camera_up = [0.0, 1.0, 0.0];
 
 document.addEventListener('keydown', (e) => {
@@ -50,8 +51,8 @@ document.addEventListener('keydown', (e) => {
     if(camera_angles.omega > CAMERA_TOP_OMEGA) camera_angles.omega = CAMERA_TOP_OMEGA;
     radius = CAMERA_SPHERE_RADIUS/camera_depth;
     camera_pos[0] = radius * Math.sin(utils.degToRad(camera_angles.omega)) * Math.cos(utils.degToRad(camera_angles.phi));
-    camera_pos[1] = Y_DEFAULT + radius * Math.cos(utils.degToRad(camera_angles.omega));
-    camera_pos[2] = radius * Math.sin(utils.degToRad(camera_angles.omega)) * Math.sin(utils.degToRad(camera_angles.phi));
+    camera_pos[1] = Y_WHEEL + radius * Math.cos(utils.degToRad(camera_angles.omega));
+    camera_pos[2] = Z_WHEEL + radius * Math.sin(utils.degToRad(camera_angles.omega)) * Math.sin(utils.degToRad(camera_angles.phi));
 
     /*console.log(camera_pos[0])
     console.log(camera_pos[1])
