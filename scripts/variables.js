04262bb1654de9f91395201b312bf393f350b617
Gabriele Stucchi <gabriele.stucchi@mail.polimi.it>
2022-01-08 09:51:15 +0100

    raycast (not working)

--- scripts/variables.js
+++ scripts/variables.js
@@ -32,6 +32,8 @@ var occlusions = [];
 var worldMatrix;
 var viewMatrix;
 var projectionMatrix;
+var buttonRaycastPlane;
+var raycastCoordinates;
 
 const colors = [
   [1, 0.482, 0, 1.0], // 0 orange
@@ -65,3 +67,33 @@ const CHORDS_text = [
 
 const degree = [0, 2, 4, 5, 7, 9, 11] // major
 const ROOTS = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+
+var raycast = {
+  dot: function (a, b) {
+      return a.map((x, i) => a[i] * b[i]).reduce((m, n) => m + n)
+  },
+  addVector: function (a, b){
+  return a.map((e,i) => e + b[i]);
+  },
+  scaleVector: function (vec, s){
+      return vec.map((e) => e * s);
+  },
+
+  // fast and easy algorithm for calculating the intersection between a plane and a line.
+  // Note: the line cannot be coplanar with the plane
+  linePlaneIntersection: function (planePoints, rayPoint1, rayDirection) {
+      // l_a = rayPoint1
+      // l_b = rayPoint2
+      // l_ab = rayDir
+      let rayPoint2 = this.addVector(rayDirection, rayPoint1);
+      let rayDir = utils.subtractVectors(rayPoint2, rayPoint1);
+      let planeDir01 = utils.subtractVectors(planePoints.p1, planePoints.p0);
+      let planeDir02 = utils.subtractVectors(planePoints.p2, planePoints.p0);
+
+      let planeCross = utils.cross(planeDir01, planeDir02);
+      let numerator = this.dot(planeCross, utils.subtractVectors(rayPoint1, planePoints.p0));
+      let denominator = this.dot(utils.subtractVectors([0, 0, 0], rayDir), planeCross);
+      let t = numerator / denominator;
+      return this.addVector(rayPoint1, this.scaleVector(rayDir, t));
+  }
+}
