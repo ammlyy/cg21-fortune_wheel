14c89fe073a06a35bb84f2a313e6871456c8e8be
Matteo <amerenamatteo@gmail.com>
2022-01-09 10:41:00 +0100

    help

--- scripts/audio.js
+++ scripts/audio.js
@@ -2,7 +2,7 @@ var progression = []
 var step = 0
 const piano = SampleLibrary.load({
     instruments: "piano",
-    baseUrl: 'cg21-fortune_wheel/samples/'
+    baseUrl: '/samples/'
 }).toDestination();
 
 const sampler = new Tone.Sampler({
